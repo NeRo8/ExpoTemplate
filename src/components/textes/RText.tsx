f9f8ce5afc55c5c57781067c2f67bc0ade9a7938
Artur Duchenko <artur.duchenko@gmail.com>
2025-11-24 11:01:43 +0200

    feat: add text types

--- src/components/textes/RText.tsx
+++ src/components/textes/RText.tsx
@@ -2,22 +2,67 @@ import { Text, TextProps } from 'react-native';
 
 import { useStyles, createStyleSheet } from 'react-native-unistyles';
 
-interface RTextProps extends TextProps {}
+type TextType =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'body'
+  | 'caption'
+  | 'label';
+
+interface RTextProps extends TextProps {
+  type: TextType;
+}
 
 export const CustomText = ({ children, ...props }: RTextProps) => {
-  const { styles } = useStyles(stylesheet);
+  const { styles } = useStyles(stylesheet, { type: props.type });
   return (
-    <Text {...props} style={[styles.text, props.style]}>
+    <Text {...props} style={[styles.textStyle, props.style]}>
       {children}
     </Text>
   );
 };
 
 const stylesheet = createStyleSheet((theme) => ({
-  text: {
+  textStyle: {
     color: theme.colors.paragraph,
     fontSize: theme.fontSizes.md,
     fontWeight: theme.fontWeights.regular,
     fontFamily: theme.fonts.regular,
+    variants: {
+      type: {
+        h1: {
+          fontSize: theme.fontSizes.xxxl,
+          fontWeight: theme.fontWeights.bold,
+        },
+        h2: {
+          fontSize: theme.fontSizes.xxl,
+        },
+        h3: {
+          fontSize: theme.fontSizes.xl,
+        },
+        h4: {
+          fontSize: theme.fontSizes.lg,
+        },
+        h5: {
+          fontSize: theme.fontSizes.md,
+        },
+        h6: {
+          fontSize: theme.fontSizes.sm,
+        },
+        body: {
+          fontSize: theme.fontSizes.md,
+        },
+        caption: {
+          fontSize: theme.fontSizes.sm,
+        },
+        label: {
+          fontSize: theme.fontSizes.xs,
+        },
+      },
+    },
   },
 }));
--- src/screens/SplashScreen/index.tsx
+++ src/screens/SplashScreen/index.tsx
@@ -8,7 +8,15 @@ export const SplashScreenPage = () => {
   const { styles } = useStyles(stylesheet);
   return (
     <View style={styles.pageStyle}>
-      <CustomText>SplashScreen</CustomText>
+      <CustomText type="h1">SplashScreen</CustomText>
+      <CustomText type="h2">SplashScreen</CustomText>
+      <CustomText type="h3">SplashScreen</CustomText>
+      <CustomText type="h4">SplashScreen</CustomText>
+      <CustomText type="h5">SplashScreen</CustomText>
+      <CustomText type="h6">SplashScreen</CustomText>
+      <CustomText type="body">SplashScreen</CustomText>
+      <CustomText type="caption">SplashScreen</CustomText>
+      <CustomText type="label">SplashScreen</CustomText>
     </View>
   );
 };
