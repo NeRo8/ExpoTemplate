f617bc96ec0e68117ba0f5d241a7fd4052685e7c
Artur Duchenko <artur.duchenko@gmail.com>
2025-11-24 15:02:49 +0200

    fix: change header title

--- src/screens/MainScreen/widgets/Header.tsx
+++ src/screens/MainScreen/widgets/Header.tsx
@@ -5,6 +5,8 @@ import { View } from 'react-native';
 import { useVideoPlayer, VideoView } from 'expo-video';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
+import { CustomText } from '@components/textes/CustomText';
+
 import { videos } from '@constants/videos';
 
 import { SearchInput } from './SearchInput';
@@ -26,16 +28,37 @@ export const Header = () => {
         player={player}
         nativeControls={false}
         contentFit="cover"
-        style={styles.containerStyle}
+        style={styles.videoContainerStyle}
       />
+      <CustomText style={styles.headerTitleStyle}>
+        YOUR{' '}
+        <CustomText style={styles.headerBoldTitleStyle}>
+          GATE TO DEVELOPMENT
+        </CustomText>
+      </CustomText>
       <SearchInput />
     </View>
   );
 };
 
-const stylesheet = createStyleSheet({
-  containerStyle: {
+const stylesheet = createStyleSheet((theme) => ({
+  videoContainerStyle: {
     width: '100%',
     height: 250,
   },
-});
+  headerTitleStyle: {
+    position: 'absolute',
+    zIndex: 100,
+    bottom: 100,
+    left: 20,
+    right: 20,
+    textAlign: 'center',
+    fontSize: 24,
+    color: theme.colors.white,
+  },
+  headerBoldTitleStyle: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: theme.colors.white,
+  },
+}));
